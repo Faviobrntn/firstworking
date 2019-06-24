4b3087ddc293dda2c5cfbe47fe350aacecd00297
Favio <faviobarnatan@gmail.com>
2019-06-24 19:31:06 -0300

    modal login

--- vistas/paginas/index.php
+++ vistas/paginas/index.php
@@ -12,6 +12,8 @@
     <!-- Custom styles for this template -->
     <link rel="stylesheet" href="<?=HOST?>vendor/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?=HOST?>vendor/css/scrolling-nav.css">
+    <link rel="stylesheet" href="<?=HOST?>vendor/css/mdb.min.css">
+    <link rel="stylesheet" href="<?=HOST?>vendor/css/style.min.css">
 
 </head>
 
@@ -178,126 +180,10 @@
 
 
     <!---------------------------------------------------LOGIN------------------------------------------------->
-    <section id="idFormLogin">
-        <!--Modal: Login / Register Form-->
-        <div class="modal fade" id="modalLRForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog cascading-modal" role="document">
-                <!--Content-->
-                <div class="modal-content">
+<!--     <section id="idFormLogin">
+        
 
-                    <!--Modal cascading tabs-->
-                    <div class="modal-c-tabs">
-
-                        <!-- Nav tabs -->
-                        <ul class="nav nav-tabs md-tabs tabs-2 light-blue darken-3" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link active" data-toggle="tab" href="#panel7" role="tab"><i
-                                        class="fas fa-user mr-1"></i>
-                                    Acceder</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" data-toggle="tab" href="#panel8" role="tab"><i
-                                        class="fas fa-user-plus mr-1"></i>
-                                    Registrarse</a>
-                            </li>
-                        </ul>
-
-                        <!-- Tab panels -->
-                        <div class="tab-content">
-                            <!--Panel 7-->
-                            <div class="tab-pane fade in show active" id="panel7" role="tabpanel">
-
-                                <!--Body-->
-                                <div class="modal-body mb-1">
-                                    <div class="md-form form-sm mb-5">
-                                        <i class="fas fa-envelope prefix"></i>
-                                        <input type="email" id="modalLRInput10"
-                                            class="form-control form-control-sm validate">
-                                        <label data-error="wrong" data-success="right" for="modalLRInput10">Tu correo
-                                            electronico</label>
-                                    </div>
-
-                                    <div class="md-form form-sm mb-4">
-                                        <i class="fas fa-lock prefix"></i>
-                                        <input type="password" id="modalLRInput11"
-                                            class="form-control form-control-sm validate">
-                                        <label data-error="wrong" data-success="right" for="modalLRInput11">Tu
-                                            contraseña</label>
-                                    </div>
-                                    <div class="text-center mt-2">
-                                        <button class="btn btn-info">Acceder<i class="fas fa-sign-in ml-1"></i></button>
-                                    </div>
-                                </div>
-                                <!--Footer-->
-                                <div class="modal-footer">
-                                    <div class="options text-center text-md-right mt-1">
-                                        <p>No eres miembro? <a href="#" class="blue-text">Registrate</a></p>
-                                        <p>Olvidaste la <a href="#" class="blue-text">Contraseña?</a></p>
-                                    </div>
-                                    <button type="button" class="btn btn-outline-info waves-effect ml-auto"
-                                        data-dismiss="modal">Cerrar</button>
-                                </div>
-
-                            </div>
-                            <!--/.Panel 7-->
-
-                            <!--Panel 8-->
-                            <div class="tab-pane fade" id="panel8" role="tabpanel">
-
-                                <!--Body-->
-                                <div class="modal-body">
-                                    <div class="md-form form-sm mb-5">
-                                        <i class="fas fa-envelope prefix"></i>
-                                        <input type="email" id="modalLRInput12"
-                                            class="form-control form-control-sm validate">
-                                        <label data-error="wrong" data-success="right" for="modalLRInput12">Tu correo
-                                            electronico</label>
-                                    </div>
-
-                                    <div class="md-form form-sm mb-5">
-                                        <i class="fas fa-lock prefix"></i>
-                                        <input type="password" id="modalLRInput13"
-                                            class="form-control form-control-sm validate">
-                                        <label data-error="wrong" data-success="right" for="modalLRInput13">Tu
-                                            contraseña</label>
-                                    </div>
-
-                                    <div class="md-form form-sm mb-4">
-                                        <i class="fas fa-lock prefix"></i>
-                                        <input type="password" id="modalLRInput14"
-                                            class="form-control form-control-sm validate">
-                                        <label data-error="wrong" data-success="right" for="modalLRInput14">Repite la
-                                            contraseña</label>
-                                    </div>
-
-                                    <div class="text-center form-sm mt-2">
-                                        <button class="btn btn-info">Registrate<i
-                                                class="fas fa-sign-in ml-1"></i></button>
-                                    </div>
-
-                                </div>
-                                <!--Footer-->
-                                <div class="modal-footer">
-                                    <div class="options text-right">
-                                        <p class="pt-1">Ya tienes una cuenta? <a href="#" class="blue-text">Acceder</a>
-                                        </p>
-                                    </div>
-                                    <button type="button" class="btn btn-outline-info waves-effect ml-auto"
-                                        data-dismiss="modal">Cerrar</button>
-                                </div>
-                            </div>
-                            <!--/.Panel 8-->
-                        </div>
-
-                    </div>
-                </div>
-                <!--/.Content-->
-            </div>
-        </div>
-        <!--Modal: Login / Register Form-->
-
-    </section>
+    </section> -->
 
 
     <section id="about">
@@ -372,6 +258,126 @@
 
     <!-- Custom JavaScript for this theme -->
     <script src="<?=HOST?>vendor/js/scrolling-nav.js"></script>
+
+
+
+    <!--Modal: Login / Register Form-->
+    <div class="modal fade" id="modalLRForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog cascading-modal" role="document">
+            <!--Content-->
+            <div class="modal-content">
+
+                <!--Modal cascading tabs-->
+                <div class="modal-c-tabs">
+
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-tabs md-tabs tabs-2 light-blue darken-3" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#panel7" role="tab"><i
+                                    class="fas fa-user mr-1"></i>
+                                Acceder</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#panel8" role="tab"><i
+                                    class="fas fa-user-plus mr-1"></i>
+                                Registrarse</a>
+                        </li>
+                    </ul>
+
+                    <!-- Tab panels -->
+                    <div class="tab-content">
+                        <!--Panel 7-->
+                        <div class="tab-pane fade in show active" id="panel7" role="tabpanel">
+
+                            <!--Body-->
+                            <div class="modal-body mb-1">
+                                <div class="md-form form-sm mb-5">
+                                    <i class="fas fa-envelope prefix"></i>
+                                    <input type="email" id="modalLRInput10"
+                                        class="form-control form-control-sm validate">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput10">Tu correo
+                                        electronico</label>
+                                </div>
+
+                                <div class="md-form form-sm mb-4">
+                                    <i class="fas fa-lock prefix"></i>
+                                    <input type="password" id="modalLRInput11"
+                                        class="form-control form-control-sm validate">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput11">Tu
+                                        contraseña</label>
+                                </div>
+                                <div class="text-center mt-2">
+                                    <button class="btn btn-info">Acceder<i class="fas fa-sign-in ml-1"></i></button>
+                                </div>
+                            </div>
+                            <!--Footer-->
+                            <div class="modal-footer">
+                                <div class="options text-center text-md-right mt-1">
+                                    <p>No eres miembro? <a href="#" class="blue-text">Registrate</a></p>
+                                    <p>Olvidaste la <a href="#" class="blue-text">Contraseña?</a></p>
+                                </div>
+                                <button type="button" class="btn btn-outline-info waves-effect ml-auto"
+                                    data-dismiss="modal">Cerrar</button>
+                            </div>
+
+                        </div>
+                        <!--/.Panel 7-->
+
+                        <!--Panel 8-->
+                        <div class="tab-pane fade" id="panel8" role="tabpanel">
+
+                            <!--Body-->
+                            <div class="modal-body">
+                                <div class="md-form form-sm mb-5">
+                                    <i class="fas fa-envelope prefix"></i>
+                                    <input type="email" id="modalLRInput12"
+                                        class="form-control form-control-sm validate">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput12">Tu correo
+                                        electronico</label>
+                                </div>
+
+                                <div class="md-form form-sm mb-5">
+                                    <i class="fas fa-lock prefix"></i>
+                                    <input type="password" id="modalLRInput13"
+                                        class="form-control form-control-sm validate">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput13">Tu
+                                        contraseña</label>
+                                </div>
+
+                                <div class="md-form form-sm mb-4">
+                                    <i class="fas fa-lock prefix"></i>
+                                    <input type="password" id="modalLRInput14"
+                                        class="form-control form-control-sm validate">
+                                    <label data-error="wrong" data-success="right" for="modalLRInput14">Repite la
+                                        contraseña</label>
+                                </div>
+
+                                <div class="text-center form-sm mt-2">
+                                    <button class="btn btn-info">Registrate<i
+                                            class="fas fa-sign-in ml-1"></i></button>
+                                </div>
+
+                            </div>
+                            <!--Footer-->
+                            <div class="modal-footer">
+                                <div class="options text-right">
+                                    <p class="pt-1">Ya tienes una cuenta? <a href="#" class="blue-text">Acceder</a>
+                                    </p>
+                                </div>
+                                <button type="button" class="btn btn-outline-info waves-effect ml-auto"
+                                    data-dismiss="modal">Cerrar</button>
+                            </div>
+                        </div>
+                        <!--/.Panel 8-->
+                    </div>
+
+                </div>
+            </div>
+            <!--/.Content-->
+        </div>
+    </div>
+    <!--Modal: Login / Register Form-->
 
 </body>
 
