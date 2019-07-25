b050399fff8611d936f0f9922f146dbe33ab9422
Lucas Pavan <lucaspavan.lp@gmail.com>
2019-07-25 20:10:22 -0300

    backend busqueda

---
+++ controladores/busquedas.php
@@ -0,0 +1,26 @@
+<?php  
+// namespace Controladores;
+// use  Modelos\Usuario;
+
+class Carreras extends Controlador
+{
+    public function __construct() {
+        parent::__construct();
+        $this->loadModel('Carrera');
+
+        $this->autorizacion();
+    }
+
+    public function index()
+    {
+        if (!empty($_GET['search'])) {
+            $ofertas = $this->Busqueda->buscar($_GET['search']);
+        }
+
+        #$this->set(compact('carreras', 'facultades'));
+        $this->render('ofertas/index');
+    }
+
+}
+
+?>
