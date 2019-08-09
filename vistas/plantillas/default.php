265efaef9a019426fa9ce2f19dc52eaf3f91b491
Favio <faviobarnatan@gmail.com>
2019-08-09 00:41:39 -0300

    navegacion en el admin

--- vistas/plantillas/default.php
+++ vistas/plantillas/default.php
@@ -46,51 +46,45 @@
         <div class="container-fluid">
 
             <!-- Brand -->
-            <a class="navbar-brand waves-effect" href="https://mdbootstrap.com/docs/jquery/" target="_blank">
-            <strong class="blue-text">MDB</strong>
+            <a class="navbar-brand waves-effect" href="<?=HOST?>" target="_blank">
+                <strong class="blue-text">Firstworking</strong>
             </a>
 
             <!-- Collapse -->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
             </button>
 
             <!-- Links -->
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-            <!-- Left -->
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link waves-effect" href="#">Home
-                        <span class="sr-only">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link waves-effect" href="https://mdbootstrap.com/docs/jquery/" target="_blank">About
-                    MDB</a>
-                </li>
-            </ul>
+                <!-- Left -->
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link waves-effect" href="<?=HOST?>">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect" href="<?=HOST?>#about" target="_blank">Nosotros</a>
+                    </li>
+                </ul>
 
-            <!-- Right -->
-            <ul class="navbar-nav nav-flex-icons">
-                <li class="nav-item">
-                <a href="https://www.facebook.com/mdbootstrap" class="nav-link waves-effect" target="_blank">
-                    <i class="fab fa-facebook-f"></i>
-                </a>
-                </li>
-                <li class="nav-item">
-                <a href="https://twitter.com/MDBootstrap" class="nav-link waves-effect" target="_blank">
-                    <i class="fab fa-twitter"></i>
-                </a>
-                </li>
-                <li class="nav-item">
-                <a href="https://github.com/mdbootstrap/bootstrap-material-design" class="nav-link border border-light rounded waves-effect"
-                    target="_blank">
-                    <i class="fab fa-github mr-2"></i>MDB GitHub
-                </a>
-                </li>
-            </ul>
+                <!-- Right -->
+                <ul class="navbar-nav nav-flex-icons">
+                    <!-- <li class="nav-item">
+                        <a href="https://twitter.com/MDBootstrap" class="nav-link waves-effect" target="_blank">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li> -->
+                    <li class="nav-item">
+                    <a href="<?=HOST?>usuarios/perfil" class="nav-link border border-light rounded waves-effect"
+                        target="_blank">
+                        <i class="fas fa-user mr-2"></i>Mi Perfil
+                    </a>
+                    </li>
+                </ul>
 
             </div>
 
@@ -101,7 +95,7 @@
         <!-- Sidebar -->
         <div class="sidebar-fixed position-fixed">
 
-        <a href="/" class="logo-wrapper waves-effect bg-dark mb-4">
+        <a href="<?=HOST?>" class="logo-wrapper waves-effect bg-dark mb-4">
             <img src="<?=HOST?>Untitled.png" class="img-fluid" alt="">
         </a>
 
